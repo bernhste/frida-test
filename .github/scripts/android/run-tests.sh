7d0dd9d909ac9b79ae06d6b875998da1d6a4696e
bernhste <s.bernhardsgruetter@proton.me>
2026-09-17 07:07:22 +0000

    fix: issue with test script running on github pipeline

---
+++ .github/scripts/android/run-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+npm run test:android -- -o test-results.json > test-output-android.log 2>&1
+status=$?
+
+cat test-output-android.log
+
+{
+  echo "### Test host Android"
+  echo '```'
+  cat test-output-android.log
+  echo '```'
+} >> "$GITHUB_STEP_SUMMARY"
+
+exit $status
